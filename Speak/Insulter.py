da7d1d91498c215ea24df7f5431303aa281c1cae
tyunkeow <urlaub.harms@web.de>
2014-01-19 12:31:54 +0100

    reformat

--- Speak/Insulter.py
+++ Speak/Insulter.py
@@ -7,175 +7,175 @@ from Text2Wav import text2wav, playWav
 
 
 ins_data = {
-"adjektive": [
+    "adjektive": [
         {
-                'm': "schoener",
-                "f": "schoene",
-                "n": "schoenes"
+            'm': "schoener",
+            "f": "schoene",
+            "n": "schoenes"
         },
         {
-                "m": "buckliger",
-                "f": "bucklige",
-                "n": "buckliges"
+            "m": "buckliger",
+            "f": "bucklige",
+            "n": "buckliges"
         },
         {
-                "m": "besoffener",
-                "f": "besoffene",
-                "n": "besoffenes"
+            "m": "besoffener",
+            "f": "besoffene",
+            "n": "besoffenes"
         },
         {
-                "m": "fieser",
-                "f": "fiese",
-                "n": "fieses"
+            "m": "fieser",
+            "f": "fiese",
+            "n": "fieses"
         },
         {
-                "m": "tollpatschiger",
-                "f": "tollpatschige",
-                "n": "tollpatschiges"
+            "m": "tollpatschiger",
+            "f": "tollpatschige",
+            "n": "tollpatschiges"
         },
         {
-                "m": "grosser",
-                "f": "grosse",
-                "n": "grosses"
+            "m": "grosser",
+            "f": "grosse",
+            "n": "grosses"
         },
         {
-                "m": "unverschaemter",
-                "f": "unverschaemte",
-                "n": "unverschaemtes"
+            "m": "unverschaemter",
+            "f": "unverschaemte",
+            "n": "unverschaemtes"
         },
         {
-                "m": "vorlauter",
-                "f": "vorlaute",
-                "n": "vorlautes"
+            "m": "vorlauter",
+            "f": "vorlaute",
+            "n": "vorlautes"
         },
         {
-                "m": "brutaler",
-                "f": "brutale",
-                "n": "brutales"
+            "m": "brutaler",
+            "f": "brutale",
+            "n": "brutales"
         },
         {
-                "m": "schlechter",
-                "f": "schlechte",
-                "n": "schlechters"
+            "m": "schlechter",
+            "f": "schlechte",
+            "n": "schlechters"
         },
         {
-                "m": "gewissenloser",
-                "f": "gewissenlose",
-                "n": "gewissenloses"
+            "m": "gewissenloser",
+            "f": "gewissenlose",
+            "n": "gewissenloses"
         },
         {
-                "m": "ruecksichtsloser",
-                "f": "ruecksichtslose",
-                "n": "ruecksichtsloses"
+            "m": "ruecksichtsloser",
+            "f": "ruecksichtslose",
+            "n": "ruecksichtsloses"
         },
         {
-                "m": "bemitleidenswerter",
-                "f": "bemitleidenswerte",
-                "n": "bemitleidenswertes"
+            "m": "bemitleidenswerter",
+            "f": "bemitleidenswerte",
+            "n": "bemitleidenswertes"
         },
         {
-                "m": "haariger",
-                "f": "haarige",
-                "n": "haariges"
+            "m": "haariger",
+            "f": "haarige",
+            "n": "haariges"
         },
         {
-                "m": "verstrahlter",
-                "f": "verstrahlte",
-                "n": "verstrahltes"
+            "m": "verstrahlter",
+            "f": "verstrahlte",
+            "n": "verstrahltes"
         },
         {
-                "m": "kleiner",
-                "f": "kleine",
-                "n": "kleines"
+            "m": "kleiner",
+            "f": "kleine",
+            "n": "kleines"
         }
-],
-"steigerungen" : [ "leicht", "sehr", "massiv", "extrem", "besonders", "voll", "super" ],
-"substantive": [
+    ],
+    "steigerungen": ["leicht", "sehr", "massiv", "extrem", "besonders", "voll", "super"],
+    "substantive": [
         {
-                "geschlecht": "m",
-                "wert": "Affe"
+            "geschlecht": "m",
+            "wert": "Affe"
         },
         {
-                "geschlecht": "n",
-                "wert": "Stinktier"
+            "geschlecht": "n",
+            "wert": "Stinktier"
         },
         {
-                "geschlecht": "m",
-                "wert": "Hosch"
+            "geschlecht": "m",
+            "wert": "Hosch"
         },
         {
-                "geschlecht": "m",
-                "wert": "Heckenpenner"
+            "geschlecht": "m",
+            "wert": "Heckenpenner"
         },
         {
-                "geschlecht": "m",
-                "wert": "Asi"
+            "geschlecht": "m",
+            "wert": "Asi"
         },
         {
-                "geschlecht": "m",
-                "wert": "Luegenbaron"
+            "geschlecht": "m",
+            "wert": "Luegenbaron"
         },
         {
-                "geschlecht": "m",
-                "wert": "Doedel"
+            "geschlecht": "m",
+            "wert": "Doedel"
         },
         {
-                "geschlecht": "m",
-                "wert": "Erbsenzaehler"
+            "geschlecht": "m",
+            "wert": "Erbsenzaehler"
         },
         {
-                "geschlecht": "m",
-                "wert": "Ruepel"
+            "geschlecht": "m",
+            "wert": "Ruepel"
         },
         {
-                "geschlecht": "n",
-                "wert": "Schwein"
+            "geschlecht": "n",
+            "wert": "Schwein"
         },
         {
-                "geschlecht": "n",
-                "wert": "Ferkel"
+            "geschlecht": "n",
+            "wert": "Ferkel"
         },
         {
-                "geschlecht": "n",
-                "wert": "Froschgesicht"
+            "geschlecht": "n",
+            "wert": "Froschgesicht"
         },
         {
-                "geschlecht": "n",
-                "wert": "Pupsgesicht"
+            "geschlecht": "n",
+            "wert": "Pupsgesicht"
         },
         {
-                "geschlecht": "n",
-                "wert": "Huhn"
+            "geschlecht": "n",
+            "wert": "Huhn"
         },
         {
-                "geschlecht": "f",
-                "wert": "Hexe"
+            "geschlecht": "f",
+            "wert": "Hexe"
         },
         {
-                "geschlecht": "m",
-                "wert": "Sesselfurzer"
+            "geschlecht": "m",
+            "wert": "Sesselfurzer"
         },
         {
-                "geschlecht": "f",
-                "wert": "Sesselfurzerin"
+            "geschlecht": "f",
+            "wert": "Sesselfurzerin"
         },
         {
-                "geschlecht": "f",
-                "wert": "Sau"
+            "geschlecht": "f",
+            "wert": "Sau"
         },
         {
-                "geschlecht": "f",
-                "wert": "Kartoffelnase"
+            "geschlecht": "f",
+            "wert": "Kartoffelnase"
         },
         {
-                "geschlecht": "f",
-                "wert": "Flachzange"
+            "geschlecht": "f",
+            "wert": "Flachzange"
         },
         {
-                "geschlecht": "f",
-                "wert": "Dumpfbacke"
+            "geschlecht": "f",
+            "wert": "Dumpfbacke"
         }
-]
+    ]
 }
 
 #f = open("/home/pi/python/insults2.json", 'w')
@@ -188,35 +188,42 @@ ins_data = {
 
 filename = '/home/pi/data/audio/insult{}.wav'
 
+
 def pickRandom(list):
     return 0
 
+
 def pickAdjektiv(g, idx):
     arr = ins_data['adjektive']
-    rnd = random.randint(0, len(arr)-1)
+    rnd = random.randint(0, len(arr) - 1)
     return arr[rnd][g]
+
 
 def pickSubstantiv(idx):
     arr = ins_data['substantive']
     if idx < 0:
-        rnd = random.randint(0, len(arr)-1)
+        rnd = random.randint(0, len(arr) - 1)
     return (arr[rnd]['wert'], arr[rnd]['geschlecht'])
+
 
 def pickSteigerung(g, idx):
     arr = ins_data['steigerungen']
-    rnd = random.randint(0, len(arr)-1)
+    rnd = random.randint(0, len(arr) - 1)
     return arr[rnd]
+
 
 def get_insult(idx_steig, idx_adj, idx_sub):
     subst, g = pickSubstantiv(idx_sub)
-    return "Du " + pickSteigerung(g, idx_steig) + " " + pickAdjektiv(g, idx_adj) + " " + subst 
+    return "Du " + pickSteigerung(g, idx_steig) + " " + pickAdjektiv(g, idx_adj) + " " + subst
+
 
 def speak_next_insult():
-    max = len(ins_data['steigerungen'])*len(ins_data['adjektive'])*len(ins_data['substantive'])
+    max = len(ins_data['steigerungen']) * len(ins_data['adjektive']) * len(ins_data['substantive'])
     fn = filename.format(random.randint(0, max))
     print "speaking insult " + str(fn)
     playWav(fn)
- 
+
+
 def create_insult_audio_db():
     i = 0
     for steig in ins_data['steigerungen']:
@@ -231,7 +238,7 @@ def create_insult_audio_db():
                 text = "Du {} {} {}".format(steigerung, adjektiv, substantiv)
                 #print "Writing {} to {}".format(text, filename)
                 text2wav(text, filename, 30, 1.2)
-                
+
 
 if __name__ == "__main__":
     insult = get_insult(-1, -1, -1)
