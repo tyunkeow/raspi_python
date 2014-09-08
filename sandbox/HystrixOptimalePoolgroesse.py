b05350ddc0beaa4745a06ce8aa7583124a3f2adc
tyunkeow <urlaub.harms@web.de>
2014-09-08 21:14:35 +0200

    minor changes

--- sandbox/HystrixOptimalePoolgroesse.py
+++ sandbox/HystrixOptimalePoolgroesse.py
@@ -4,7 +4,7 @@ import gmpy
 
 p = 1./84  # Anzahl server
 #N = [23.1, 46.2, 28.51, 49.875, 37.38, 49.98, 53.13, 55.75]  # thread
-N = [24, 47, 29, 50, 38, 50, 54, 56, 210]  # thread
+N = [1, 2, 3, 5, 10, 13, 21, 34, 55, 89, 144]  # thread
 
 def binomial(k, p, n):
     return scipy.misc.comb(n, k) * p**k * (1-p)**(n-k)
@@ -15,7 +15,7 @@ def binomial_exact(k, p, n):
 # Wahrscheinlichkeit, dass ein Server mehr als k-mal gezogen wurde
 def get_prob_X_groesser_k(k, p, n):
     result = 0.
-    for i in range(k):
+    for i in range(k+1):
         #print "k="+str(k)
         result += binomial(i, p, n)
     return (1. - result)
@@ -23,9 +23,9 @@ def get_prob_X_groesser_k(k, p, n):
 for n in N:
     #print "------------- n = {} ------------------".format(n)
     print ""
-    for k in range(10):
-        proz = get_prob_X_groesser_k(k, p, n) * 100
-        print "Request Count={0}; Poolsize={1}; Prob Pool exhausted={2:.20g} %".format(n, k, proz)
+    for poolSize in range(10):
+        proz = get_prob_X_groesser_k(poolSize, p, n) * 100
+        print "Request Count={0}; Poolsize={1}; Prob Pool exhausted={2:.20g} %".format(n, poolSize, proz)
         #print binomial_exact(k, p, n)
 
 
