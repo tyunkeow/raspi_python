168b02db40ce5bf0a6c78b9b4dc5ee74e46e3972
tyunkeow <urlaub.harms@web.de>
2014-09-21 20:51:46 +0200

    added initial voice output

--- speak/tinkerforge_stack.py
+++ speak/tinkerforge_stack.py
@@ -4,6 +4,7 @@ from tinkerforge.bricklet_motion_detector import MotionDetector
 from tinkerforge.bricklet_rotary_poti import RotaryPoti
 from tinkerforge.bricklet_io4 import IO4
 from insulter import speak_next_insult
+from text2wav import play_sound, AUDIO_DIR
 from time import sleep
 import sys
 
@@ -101,6 +102,7 @@ if __name__ == "__main__":
     print "Poti left position  : ", stack.poti_left.get_position()
     print "Poti right position : ", stack.poti_right.get_position()
     stack.register_callbacks()
+    play_sound(AUDIO_DIR + "hello.aiff")
 
     sleep(1000000)
     input('Press key to exit\n')
